339264dc52ea4e867402b9fa88662f07daa9e5ed
Norman Skinner <norman.skinner@live.com>
2019-06-15 23:47:35 -0700

    Passing the crypto unit tests.

--- SkinnerNR-cp130-MAIN/src/main/java/edu/uw/nrs/crypto/PrivateMessageCodecImpl.java
+++ SkinnerNR-cp130-MAIN/src/main/java/edu/uw/nrs/crypto/PrivateMessageCodecImpl.java
@@ -1,11 +1,13 @@
 package edu.uw.nrs.crypto;
 
-import java.io.ByteArrayOutputStream;
-import java.io.FileOutputStream;
+import static edu.uw.nrs.crypto.CryptoUtils.JCEKS;
+import static edu.uw.nrs.crypto.CryptoUtils.generateAesSecretKey;
+import static edu.uw.nrs.crypto.CryptoUtils.keyBytesToAesSecretKey;
+import static edu.uw.nrs.crypto.CryptoUtils.loadKeyStore;
+
 import java.io.IOException;
 import java.security.GeneralSecurityException;
 import java.security.Key;
-import java.security.KeyManagementException;
 import java.security.KeyStore;
 import java.security.PrivateKey;
 import java.security.PublicKey;
@@ -13,11 +15,7 @@ import java.security.Signature;
 import java.security.cert.Certificate;
 import java.util.Arrays;
 
-import static edu.uw.nrs.crypto.CryptoUtils.*;
-
 import javax.crypto.Cipher;
-import javax.crypto.CipherOutputStream;
-import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
 
 import org.slf4j.Logger;
@@ -72,66 +70,62 @@ public class PrivateMessageCodecImpl implements PrivateMessageCodec {
 			String recipientCertName) throws GeneralSecurityException, IOException {
 
 		byte[] encipheredSharedKey = null;
-		byte[] cipherData = null;
+		byte[] ciphertext = null;
 		byte[] signature = null;
 
-		log.debug("plaintext = " + Arrays.toString(plaintext));
-		log.debug("senderKeyStoreName = " + senderKeyStoreName.toString());
+		log.debug("plaintext as array = " + Arrays.toString(plaintext));
+		log.debug("plaintext = " + new String(plaintext));
+		log.debug("senderKeyStoreName = " + senderKeyStoreName);
 		log.debug("senderKeyStorePasswd = " + new String(senderKeyStorePasswd));
-		log.debug("senderKeyName = " + senderKeyName.toString());
+		log.debug("senderKeyName = " + senderKeyName);
 		log.debug("senderKeyPasswd = " + new String(senderKeyPasswd));
-		log.debug("senderTrustStoreName = " + senderTrustStoreName.toString());
+		log.debug("senderTrustStoreName = " + senderTrustStoreName);
 		log.debug("senderTrustStorePasswd = " + new String(senderTrustStorePasswd));
-		log.debug("recipientCertName = " + recipientCertName.toString());
+		log.debug("recipientCertName = " + recipientCertName);
 
-		
 		// 1. Generate a one-time use shared symmetric secret key
 		// slide 35
 		SecretKey secKey = generateAesSecretKey();
 
-		
 		// 2. Encipher the the order data
 		// using the one-time use shared symmetric secret key
 		// slide 35
 		Cipher cipher = Cipher.getInstance(secKey.getAlgorithm());
 		cipher.init(Cipher.ENCRYPT_MODE, secKey);
-		cipherData = cipher.doFinal(plaintext);
+		ciphertext = cipher.doFinal(plaintext);
 
-		
 		// 3. Obtain the bytes representing the one-time use shared symmetric secret key
 		// slide 35
-		encipheredSharedKey = secKey.getEncoded();
+		byte[] secKeyBytes = secKey.getEncoded();
 
-		
 		// 4. Retrieve the (broker's) public key from the provided truststore
 		// slide 30
 		KeyStore clientTrustStore = loadKeyStore(senderTrustStoreName, JCEKS, senderTrustStorePasswd);
-		Certificate cert =  clientTrustStore.getCertificate(recipientCertName);
-		PublicKey publicKey = cert.getPublicKey(); 
-		
-		
+		Certificate cert = clientTrustStore.getCertificate(recipientCertName);
+		PublicKey pubKey = cert.getPublicKey();
+
 		// 5. Encipher the shared symmetric secret key's bytes
 		// using the public key from the truststore
-		SecretKey secEncipheredSharedKey = keyBytesToAesSecretKey(encipheredSharedKey);
+		cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+		cipher.init(Cipher.ENCRYPT_MODE, pubKey);
+		encipheredSharedKey = cipher.doFinal(secKeyBytes);
 
-		
 		// 6. Retrieve the (client's) private key from the the provided keystore
 		// slide 29
 		KeyStore privateStore = loadKeyStore(senderKeyStoreName, JCEKS, senderKeyStorePasswd);
-		PrivateKey privateKey = (PrivateKey)privateStore.getKey(senderKeyName, senderKeyPasswd);
+		PrivateKey privateKey = (PrivateKey) privateStore.getKey(senderKeyName, senderKeyPasswd);
 
-		
 		// 7. Sign the plaintext order data using the private key from the the provided
 		// keystore
 		// slide 29
-		Signature signer = Signature.getInstance("MD5withRSA"); 
+		Signature signer = Signature.getInstance("MD5withRSA");
 		signer.initSign(privateKey);
-		signer.update(cipherData);
+		signer.update(plaintext);
 		signature = signer.sign();
 
 		// 8. Construct and return a PrivateMessageTriple containing the ciphertext, key
 		// bytes and signature
-		PrivateMessageTriple returnPMT = new PrivateMessageTriple(encipheredSharedKey, cipherData, signature);
+		PrivateMessageTriple returnPMT = new PrivateMessageTriple(encipheredSharedKey, ciphertext, signature);
 
 		return returnPMT;
 	}
@@ -177,17 +171,47 @@ public class PrivateMessageCodecImpl implements PrivateMessageCodec {
 
 		// 1. Obtain the shared secret key, order data ciphertext and signature from the
 		// provided PrivateMessageTriple
+		byte[] encipheredSharedKey = triple.getEncipheredSharedKey();
+		byte[] ciphertext = triple.getCiphertext();
+		byte[] signature = triple.getSignature();
+
 		// 2. Retrieve the (brokers's) private key from the the provided keystore
+		KeyStore keyStore = loadKeyStore(recipientKeyStoreName, JCEKS, recipientKeyStorePasswd);
+		Key privKey = keyStore.getKey(recipientKeyName, recipientKeyPasswd);
+
 		// 3. Use the private key from the keystore to decipher the shared secret key's
-		// bytes
+		Cipher decipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+		decipher.init(Cipher.DECRYPT_MODE, privKey);
+		byte[] secKeyBytes = decipher.doFinal(encipheredSharedKey);
+
 		// 4. Reconstruct the shared secret key from shared secret key's bytes
+		SecretKey secKey = keyBytesToAesSecretKey(secKeyBytes);
+
 		// 5. Use the shared secret key to decipher the order data ciphertext
+		Cipher decipherData = Cipher.getInstance(secKey.getAlgorithm());
+		decipherData.init(Cipher.DECRYPT_MODE, secKey);
+		byte[] plaintext = decipherData.doFinal(ciphertext);
+
 		// 6. Retrieve the (client's) public key from the provided truststore
+		KeyStore clientTrustStore = loadKeyStore(trustStoreName, JCEKS, trustStorePasswd);
+		Certificate cert = clientTrustStore.getCertificate(signerCertName);
+		PublicKey publicKey = cert.getPublicKey();
+
 		// 7. Verify the order data plaintext and signature using the public key from
 		// the truststore
-		// 8. Return the order data plaintext
+		// slide 30
+		Signature verifier = Signature.getInstance("MD5withRSA");
+		verifier.initVerify(publicKey);
+		verifier.update(plaintext);
+		boolean valid = verifier.verify(signature);
 
-		return null;
+		// 8. Return the order data plaintext
+		if (valid) {
+			return plaintext;
+		} else {
+			log.warn("Data was not signed correctly! ! !");
+			return null;
+		}
 	}
 
 }
