7abb2ce28d992ad4ad9628705032c8f2e7c45ee3
Norman Skinner <norman.skinner@live.com>
2019-06-24 18:35:08 -0700

    Check Point.

--- SkinnerNR-cp130-SERVLET/src/main/java/app/QuoteApp.java
+++ SkinnerNR-cp130-SERVLET/src/main/java/app/QuoteApp.java
@@ -1,5 +1,72 @@
 package app;
 
-public class QuoteApp {
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
+public class QuoteApp {
+    private static void exec(final String symbol, final String format) {
+        HttpURLConnection conn = null;
+        String baseUrl = "http://localhost:8080/StockQuote/StockQuoteServlet";
+        try {
+            String urlStr = String.format("%s?symbol=%s&format=%s", baseUrl, symbol, format);
+            System.out.println(urlStr);
+            URL url = new URL(urlStr);
+            conn = (HttpURLConnection)url.openConnection();
+            conn.setRequestMethod("GET");            
+            
+            System.out.printf("Content-Type: %s%n", conn.getContentType());
+            InputStream in = conn.getInputStream();
+            Reader rdr = new InputStreamReader(in);
+            char buf[] = new char[1024];
+            int len = 0;
+            while ((len = rdr.read(buf)) != -1) {
+                System.out.print(new String(buf, 0, len));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * @param args
+     * @throws IOException 
+     */
+    public static void main(String[] args) {
+        String courtesyType = "goog";
+        
+        System.out.println("================================================================================");
+        System.out.println("JSON:");
+        System.out.println("--------------------------------------------------------------------------------");
+        exec(courtesyType, "json");
+        System.out.println();
+        System.out.println("================================================================================");
+        System.out.println();
+        System.out.println("================================================================================");
+        System.out.println("plain:");
+        System.out.println("--------------------------------------------------------------------------------");
+        exec(courtesyType, "plain");
+        System.out.println();
+        System.out.println("================================================================================");
+        System.out.println();
+        System.out.println("================================================================================");
+        System.out.println("HTML:");
+        System.out.println("--------------------------------------------------------------------------------");
+        exec(courtesyType, "html");
+        System.out.println();
+        System.out.println("================================================================================");
+        System.out.println();
+        System.out.println("================================================================================");
+        System.out.println("XML:");
+        System.out.println("--------------------------------------------------------------------------------");
+        exec(courtesyType, "xml");
+        System.out.println();
+        System.out.println("================================================================================");
+        
+        System.out.println();
+        System.out.println();
+    }
 }
---
+++ SkinnerNR-cp130-SERVLET/src/main/java/edu/uw/nrs/web/StockQuoteTransformer.java
@@ -0,0 +1,98 @@
+package edu.uw.nrs.web;
+
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletResponse;
+
+public class StockQuoteTransformer implements Filter {
+	private static final int PRICE_INDEX = 2;
+	private static final int SYMBOL_INDEX = 1;
+	private static final int FORMAT_INDEX = 0;
+	private ServletContext ctx;
+	private String toHtmlFormat;
+	private String toJsonFormat;
+	private String toPlainFormat;
+	private String toXmlFormat;
+
+	public StockQuoteTransformer() {
+	}
+	
+	@Override
+	public void destroy() {
+	}
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+	    CharResponseWrapper wrapper = new CharResponseWrapper((HttpServletResponse)response);
+	    chain.doFilter(request, wrapper);
+
+	    String[] responseParts = wrapper.toString().split(":");
+	    
+	    String format = responseParts[FORMAT_INDEX];
+	    String symbol = responseParts[SYMBOL_INDEX];
+	    String price = responseParts[PRICE_INDEX];
+	    
+		String responseOut = "";
+		
+		switch (format) {
+		case "html":
+			responseOut = String.format(toHtmlFormat, symbol, price);
+			response.setContentType("text/html");
+			break;
+		case "json":
+			responseOut = String.format(toJsonFormat, symbol, price);
+			response.setContentType("text/plain");
+			break;
+		case "plain":
+			responseOut = String.format(toPlainFormat, symbol, price);
+			response.setContentType("text/plain");
+			break;
+		case "xml":
+			responseOut = String.format(toXmlFormat, symbol, price);
+			response.setContentType("text/xml");
+			break;
+		default:
+			responseOut = "Format was not found.";
+			response.setContentType("text/plain");
+		}
+		
+        response.setContentLength(responseOut.length());
+        response.getWriter().print(responseOut);
+	}
+	
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
+		ctx = filterConfig.getServletContext();
+        
+		toHtmlFormat = loadFormat(filterConfig, "htmlFormat");
+		toJsonFormat = loadFormat(filterConfig, "jsonFormat");
+		toPlainFormat = loadFormat(filterConfig, "plainFormat");
+		toXmlFormat = loadFormat(filterConfig, "xmlFormat");
+	}
+
+	private String loadFormat(FilterConfig filterConfig, final String format) {
+		String path = filterConfig.getInitParameter(format);
+        String realPath = ctx.getRealPath(path);
+        File f = new File(realPath);
+        try (DataInputStream dataIn = new DataInputStream(new FileInputStream(f))) {
+            byte[] bytes = new byte[(int)f.length()];
+            dataIn.readFully(bytes);
+            String readString = new String(bytes, "UTF8");
+            return readString;
+        } catch (IOException e) {
+            ctx.log("Error reading HTMP transform file.", e);
+        }
+        return null;
+	}
+}
